307fcb55b195e6a84d09f8c63a2132c3998b0e07
Matias Pierobon <matias.pierobonm@gmail.com>
2015-12-08 19:55:10 -0300

    Se inicio soporte para profiles

--- web/js/controllers/AppsController.js
+++ web/js/controllers/AppsController.js
@@ -1,6 +1,8 @@
-appInstaller.controller('AppsController', function($scope, Applications) {
+appInstaller.controller('AppsController', function($scope, Applications, Profiles) {
   $scope.apps = null;
   Applications.getAll().then(function(response){
     $scope.apps = response.data;
   });
+
+  $scope.profile = Profiles.getCurrentProfile();
 });
---
+++ web/js/services/Profiles.js
@@ -0,0 +1,29 @@
+appInstaller.service('Profiles', function($window) {
+  var $this = this;
+  this.getDefaultProfile = function () {
+      return {
+        name: 'default',
+        apps: [1,2],
+        themes: [],
+        icons: [],
+        scripts: []
+      };
+  };
+
+  this.getProfile = function (name) {
+    var profiles = $window.localStorage.getItem('profiles');
+    if(profiles == undefined){
+      $window.localStorage.setItem('profiles', [ $this.getDefaultProfile() ]);
+    }
+    return profiles.foreach(function (profile, index){
+      if(profile.name == name ){
+        return profile;
+      }
+    });
+  };
+
+  this.getCurrentProfile = function () {
+    var profile = $window.localStorage.getItem('profile');
+    return (profile == undefined) ? $this.getProfile('default') : $this.getProfile(profile);
+  };
+});
