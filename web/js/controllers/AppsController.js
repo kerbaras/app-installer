fc66adb06b6fab375a6c1e57d24851853bad6fbe
Luciano Perez Cerra <luchopcerra@gmail.com>
2015-12-10 23:28:34 -0300

    @refactor 2

--- web/js/controllers/AppsController.js
+++ web/js/controllers/AppsController.js
@@ -1,7 +1,9 @@
 appInstaller.controller('AppsController', function($scope, Applications, localStorageService) {
 
+  //@REVIEW initialization not needed
   $scope.apps = null;
   Applications.getAll().then(function(response){
+    //expected [{"id": int,"nombre": String,"descr": String,"logo":String}...]
     $scope.apps = response.data;
   });
 
@@ -28,16 +30,16 @@ appInstaller.controller('AppsController', function($scope, Applications, localSt
   $scope.exists = function (app) {
     return $scope.profiles[profile].apps.indexOf(app.id) > -1;
   };
-
+  //NOTE @REVIEW Replace forEach by Angular.forEach
   $scope.selectAll = function () {
-    $scope.apps.forEach(function (element){
+    angular.forEach($scope.apps,function(val,key){
       if($scope.profiles[profile].apps.indexOf(app.id) == -1)
         $scope.profiles[profile].apps.push(app.id);
     });
   };
-
+  //NOTE @REVIEW Replace forEach by Angular.forEach
   $scope.disableAll = function () {
-    $scope.apps.forEach(function (element){
+    angular.forEach($scope.apps,function(val,key){
       var idx = $scope.profiles[profile].apps.indexOf(app.id);
       if(idx > -1) $scope.profiles[profile].apps.splice(idx, 1);
     });
