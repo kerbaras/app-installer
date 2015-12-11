8fee7672c868ef06fe70de2ff1fe2fe0f63c8798
Luciano Perez Cerra <luchopcerra@gmail.com>
2015-12-10 22:29:35 -0300

    update URI

--- web/js/services/Applications.js
+++ web/js/services/Applications.js
@@ -1,6 +1,6 @@
 appInstaller.service('Applications', function($resource) {
   this.getAll = function () {
-      var appService = $resource('js/data/apps.json');
+      var appService = $resource('bower_components/hexeract-data-layer/apps.json');
       return appService.get().$promise;
   };
 });
